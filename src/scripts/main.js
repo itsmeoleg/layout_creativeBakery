ad98d8b53f9c89e17e978f410609be751fc06e1a
itsmeolegk <olegkuzmishchev.91@gmail.com>
2026-01-28 11:11:58 +0200

    solution for creative bakery landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const switcher = document.querySelector('.theme-switcher');
+
+switcher.addEventListener('click', () => {
+  if (page.classList.contains('page--theme--red')) {
+    page.classList.remove('page--theme--red');
+    switcher.classList.remove('theme-switcher--theme--red');
+  } else {
+    page.classList.add('page--theme--red');
+    switcher.classList.add('theme-switcher--theme--red');
+  }
+});
